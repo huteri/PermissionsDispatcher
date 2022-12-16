394313278a6aebf48ce011a1b1f0177007da44be
huteri <hut3ri@gmail.com>
2022-12-16 09:05:20 +0800

    clean build

--- ktx/src/main/java/permissions/dispatcher/ktx/PermissionRequestResult.kt
+++
@@ -1,7 +0,0 @@
-package permissions.dispatcher.ktx
-
-internal enum class PermissionResult {
-    GRANTED,
-    DENIED,
-    DENIED_AND_DISABLED
-}
--- ktx/src/main/java/permissions/dispatcher/ktx/TypeAliases.kt
+++
@@ -1,6 +0,0 @@
-package permissions.dispatcher.ktx
-
-import permissions.dispatcher.PermissionRequest
-
-internal typealias Fun = () -> Unit
-internal typealias ShowRationaleFun = (PermissionRequest) -> Unit
